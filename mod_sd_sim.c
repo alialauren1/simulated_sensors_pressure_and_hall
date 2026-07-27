32a69cc6503ffdbab61978b8a8b4fe95bf9750a8
alialauren1 <aliawolken14@gmail.com>
2026-07-27 12:01:19 -0700

    2026 07 27 AW mounting sd in init_mount_sd_open_csv working

--- mod_sd_sim.c
+++ mod_sd_sim.c
@@ -6,9 +6,30 @@
  */
 
 #include "mod_sd_sim.h"
+#include "ff.h"
+#include <stdio.h>
+#include "microsd.h"
+
+static volatile FATFS fat_fs;
 
 void init_mount_sd_open_csv(void){
- // TODO: mount sd card and open csv file
+ // TODO: open csv file and skip headers
+
+  FRESULT res;
+
+  MICROSD_Init();
+
+  res = f_mount(&fat_fs,(TCHAR*)"", 1);
+
+  if(res == (FRESULT)RES_OK)
+  {
+      printf("FATfs mount success\r\n");
+  }
+  else
+  {
+      printf("Unable to mount FAT fs.\r\n");
+  }
+
 }
 
 void read_parse_row(parsed_sensor_sample_t *out_pointer){
