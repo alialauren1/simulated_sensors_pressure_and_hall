a48c76b8a9fc6213a39ff16d099b3a324443bd36
alialauren1 <aliawolken14@gmail.com>
2026-07-27 14:33:43 -0700

    2026 07 27 AW csv open and skip-headers code added

--- mod_sd_sim.c
+++ mod_sd_sim.c
@@ -9,25 +9,44 @@
 #include "ff.h"
 #include <stdio.h>
 #include "microsd.h"
+#include <string.h>
 
 static FATFS fat_fs;
+static FIL fp;
+static FSIZE_t first_data_row_pointer;
+
+// Imported, 2025 12 05 LW: Function for converting strings (char) to UTF-8 (TCHAR) for path names
+void mod_sd_ff_encode(char* str, TCHAR* out, uint32_t len)
+{
+  uint32_t i;
+  for(i = 0; i < len; i++)
+  {
+      out[i] = ff_convert(str[i], 1);
+  }
+  out[i] = ff_convert('\0', 1);
+}
 
 void init_mount_sd_open_csv(void){
- // TODO: open csv file and skip headers
 
+  // mount sd card
   FRESULT res;
-
   MICROSD_Init();
-
   res = f_mount(&fat_fs,(TCHAR*)"", 1);
+  if(res == FR_OK) { printf("FATfs mount success\r\n"); }
+  else {printf("Unable to mount FAT fs.\r\n");}
 
-  if(res ==FR_OK)
-  {
-      printf("FATfs mount success\r\n");
+  // open csv file
+  TCHAR file_name[16];
+  mod_sd_ff_encode("data_0000.csv", file_name, strlen("data_0000.csv")); // convert string (char) to UTF-8 (TCHAR)
+  FRESULT fres = f_open(&fp, file_name, FA_READ); // open file
+  if(fres==FR_OK){
+      TCHAR header_buffer[100];
+      f_gets(header_buffer, sizeof(header_buffer), &fp);  // read string from file, skip "MOD LAB: ..." line
+      f_gets(header_buffer, sizeof(header_buffer), &fp);  // read string from file, skip "Pressure [bar],..." line
+      first_data_row_pointer = f_tell(&fp);
   }
-  else
-  {
-      printf("Unable to mount FAT fs.\r\n");
+  else {
+      printf("Issue opening and reading file, check SD card for file present\r\n");
   }
 
 }
