da914a92d9a7fc4df926ebb85f4a2ec13073f210
alialauren1 <aliawolken14@gmail.com>
2026-07-30 08:59:01 -0700

    2026 07 30 AW untrack Debug build folder

--- GNU ARM v10.3.1 - Debug/gecko_sdk_4.1.4/platform/common/src/sl_assert.d
+++
@@ -1,6 +0,0 @@
-gecko_sdk_4.1.4/platform/common/src/sl_assert.o: \
- /Users/aliawolken/SimplicityStudio/SDKs/gecko_sdk/platform/common/src/sl_assert.c \
- /Users/aliawolken/SimplicityStudio/SDKs/gecko_sdk//platform/common/toolchain/inc/sl_gcc_preinclude.h \
- /Users/aliawolken/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc/sl_assert.h
-/Users/aliawolken/SimplicityStudio/SDKs/gecko_sdk//platform/common/toolchain/inc/sl_gcc_preinclude.h:
-/Users/aliawolken/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc/sl_assert.h:
--- GNU ARM v10.3.1 - Debug/gecko_sdk_4.1.4/platform/service/system/src/sl_system_process_action.d
+++
@@ -1,6 +0,0 @@
-gecko_sdk_4.1.4/platform/service/system/src/sl_system_process_action.o: \
- /Users/aliawolken/SimplicityStudio/SDKs/gecko_sdk/platform/service/system/src/sl_system_process_action.c \
- /Users/aliawolken/SimplicityStudio/SDKs/gecko_sdk//platform/common/toolchain/inc/sl_gcc_preinclude.h \
- /Users/aliawolken/Documents/alia\ personal\ github/simulated_sensors_pressure_and_hall/autogen/sl_event_handler.h
-/Users/aliawolken/SimplicityStudio/SDKs/gecko_sdk//platform/common/toolchain/inc/sl_gcc_preinclude.h:
-/Users/aliawolken/Documents/alia\ personal\ github/simulated_sensors_pressure_and_hall/autogen/sl_event_handler.h:
